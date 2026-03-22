0068cec5bb88ff36b79402bef21c09e0149a22f6
unknown <karimm.16199@gmail.com>
2026-03-22 18:35:54 +0200

    add config file with dictionary-based settings

---
+++ src/niceshot_ai/charts_config.py
@@ -0,0 +1,17 @@
+cod_bo6_chart_config = {"charts": [
+                            {"name": "total_events_count_chart",
+                             "width": 3,
+                             "height": 10},
+                            {"name": "kd_ratio_card",
+                             "width": 1,
+                             "height": 10},
+                            {"name": "kd_timeline_chart",
+                             "width": 5,
+                             "height": 10}],
+
+                        "color_pallete": {
+                            "chart_color": '#2b2b2b', # dark grey
+                            "bezel_color": '#cc5500',
+                            "font_color": 'white',
+                            "elements_color": '#cc5500'}
+                        }
