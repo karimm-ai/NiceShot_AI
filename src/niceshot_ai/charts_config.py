be01e2bfbf3eceeda99bd5909f62fb42f40b3bbf
unknown <karimm.16199@gmail.com>
2026-05-08 05:20:33 +0300

    add charts config for cod bo7

--- src/niceshot_ai/charts_config.py
+++ src/niceshot_ai/charts_config.py
@@ -15,3 +15,23 @@ cod_bo6_chart_config = {"charts": [
                             "font_color": 'white',
                             "elements_color": '#cc5500'}
                         }
+
+
+
+cod_bo7_chart_config = {"charts": [
+                            {"name": "total_events_count_chart",
+                             "width": 3,
+                             "height": 10},
+                            {"name": "kd_ratio_card",
+                             "width": 1,
+                             "height": 10},
+                            {"name": "kd_timeline_chart",
+                             "width": 5,
+                             "height": 10}],
+
+                        "color_pallete": {
+                            "chart_color": '#2b2b2b', # dark grey
+                            "bezel_color": '#cc5500',
+                            "font_color": 'white',
+                            "elements_color": '#cc5500'}
+                        }
