f31cb582267296d3c34c93bbdc2cdc16069de81e
unknown <karimm.16199@gmail.com>
2026-05-08 05:27:44 +0300

    add events config for cod bo7

--- src/niceshot_ai/events_config.py
+++ src/niceshot_ai/events_config.py
@@ -23,3 +23,26 @@ cod_bo6_config = {
               'conf_thres': 0.82,
               'clip_eligible': True}
 }
+
+
+cod_bo7_config = {
+    "Kill": {"pre": 2,
+             "post": 2,
+             'tracker': DeepSort(max_age=3, n_init=1),
+             'cls_label': 0,
+             'conf_thres': 0.6,
+             'confirm_event': True,
+             'clip_eligible': True},
+
+    "Medal": {'tracker': DeepSort(max_age=30, nms_max_overlap=0.01),
+              'cls_label': 1,
+              'conf_thres': 0.9,
+              'confirm_event': True},
+
+    "Death": {"pre": 2,
+              "post": 1,
+              'tracker': DeathTracker(max_age=18),
+              'cls_label': 2,
+              'conf_thres': 0.82,
+              'clip_eligible': True}
+}
