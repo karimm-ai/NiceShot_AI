24acdacbd9a2ce00a37989c8ee5c4cc534aefa13
unknown <karimm.16199@gmail.com>
2026-09-11 16:04:47 +0300

    config: adjust thresholds and booleans

--- src/niceshot_ai/events_config.py
+++ src/niceshot_ai/events_config.py
@@ -11,17 +11,17 @@ cod_bo6_config = {
              'confirm_event': True,
              'clip_eligible': True},
 
-    # "Medal": {'tracker': DeepSort(max_age=30, nms_max_overlap=0.01),
-    #           'cls_label': 1,
-    #           'conf_thres': 0.9,
-    #           'confirm_event': True},
+    "Medal": {'tracker': DeepSort(max_age=30, nms_max_overlap=0.01),
+              'cls_label': 1,
+              'conf_thres': 0.9,
+              'confirm_event': True},
 
     "Death": {"pre": 3,
               "post": 1.5,
               'tracker': DeathTracker(max_age=18),
               'cls_label': 2,
               'conf_thres': 0.75,
-              'clip_eligible': False}
+              'clip_eligible': True}
 }
 
 
@@ -34,15 +34,15 @@ cod_bo7_config = {
              'confirm_event': True,
              'clip_eligible': True},
 
-    # "Medal": {'tracker': MedalTracker(),
-    #           'cls_label': 1,
-    #           'conf_thres': 0.88,
-    #           'confirm_event': True},
+    "Medal": {'tracker': MedalTracker(),
+              'cls_label': 1,
+              'conf_thres': 0.88,
+              'confirm_event': True},
 
     "Death": {"pre": 3,
               "post": 1.5,
               'tracker': DeathTracker(max_age=18),
               'cls_label': 2,
               'conf_thres': 0.8,
-              'clip_eligible': False}
+              'clip_eligible': True}
 }
