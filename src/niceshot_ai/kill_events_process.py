9fe3e3b8081646113f67c9ce5befc9ad42cf004f
unknown <karimm.16199@gmail.com>
2026-09-11 16:06:39 +0300

    refactor: improve efficiency for find_best_kills

--- src/niceshot_ai/kill_events_process.py
+++ src/niceshot_ai/kill_events_process.py
@@ -1,11 +1,9 @@
-from utils import get_duration, report_progress
 from event_types import Event
 
-import json, os
-import cv2
-from ultralytics import YOLO
-from deep_sort_realtime.deepsort_tracker import DeepSort
-from tqdm import tqdm
+import json
+import csv
+from datetime import datetime
+from bisect import bisect_left, bisect_right
 
 
 class KillEventsProcessor:
@@ -16,64 +14,42 @@ class KillEventsProcessor:
         self.output_dir = output_dir
 
 
-    def find_best_kills(self) -> dict:
-        """Kill clips where a lot of medals pop up"""
-        self.progress = set(range(3, 101, 3))
-        print("Extracting Best clips\n")
-
-        model = YOLO(self.model_path).to("cuda")
-        medal_tracker = DeepSort(max_age=30)
-        clips_medals = {}
-        kill_directories = [f"{self.output_dir}/Kill", f"{self.output_dir}/KillStreak"]
+    def find_best_kills(self) -> dict:   
         
-        for dir in kill_directories:
-            for clip in os.listdir(dir):
-                if clip.endswith("mp4"):
-                    clips_medals[f"{dir}/{clip}"] = 0
-            
-            conf_threshold = 0.85
-            temp = set()
-        
-        self.total_clips = len(clips_medals)
-        self.analyzed_clips = 0
-        
-        for key, _ in clips_medals.items():
-            cap = cv2.VideoCapture(key)
-            TOTAL_FRAMES_TO_BE_ANALYZED = get_duration(key)*60
+        def timestamp_to_seconds(timestamp):
+            t = datetime.strptime(timestamp, "%H:%M:%S")
+            return t.hour * 3600 + t.minute * 60 + t.second
 
-            with tqdm(total=TOTAL_FRAMES_TO_BE_ANALYZED, desc="Processing video", unit="frame") as pbar:
-                while cap.isOpened():
-                    ret, frame = cap.read()
-                    if not ret:
-                        break
+        with open(f"{self.output_dir}/events_temp_2.json", "r") as f:
+            events = json.load(f)
 
-                    results = model(frame, verbose=False)[0]
-                    detections = []
+        csv_timestamps = []
 
-                    for box in results.boxes:
-                        x1, y1, x2, y2 = box.xyxy[0].tolist()
-                        conf = box.conf.item()
-                        cls = int(box.cls.item())
-                        
-                        if conf>=conf_threshold and cls == 1:
-                            detections.append(([x1, y1, x2-x1, y2-y1] , conf, cls))
-                        
-                    tracks = medal_tracker.update_tracks(detections, frame=frame)
+        with open(f"{self.output_dir}/video1.csv", "r") as f:
+            reader = csv.DictReader(f)
 
-                    for track in tracks:
-                        if track.track_id not in temp:
-                            clips_medals[key]+=1
-                            temp.add(track.track_id)
+            for row in reader:
+                csv_timestamps.append(
+                    timestamp_to_seconds(row["Timestamp"])
+                )
 
-                    pbar.update(1)
-            
-            cap.release()
-            self.analyzed_clips+=1
-            report_progress(self.output_dir, self.analyzed_clips, self.total_clips, self.progress, "FINDING BEST KILLS...")
+        csv_timestamps.sort()
 
-        sorted_clips_medals = sorted(clips_medals.items(), key=lambda item: item[1], reverse=True)
-        final_clips = [clip_path for clip_path, _ in sorted_clips_medals]
-        return final_clips
+        for event in events:
+            start = event["timestart"]
+            end = event["timeend"]
+
+            left = bisect_left(csv_timestamps, start)
+            right = bisect_right(csv_timestamps, end)
+
+            event["medals_count"] = right - left
+
+        events.sort(key=lambda x: x.get("count", 0), reverse=True)
+
+        with open(f"{self.output_dir}/events_temp_2.json", "w") as f:
+            json.dump(events, f, indent=4)
+
+        return [event["desc"] for event in events]
 
 
     def concat_kill_streaks(self, video_num: int):
@@ -109,13 +85,13 @@ class KillEventsProcessor:
             kill_streaks.append(current_streak)
 
         for streak in kill_streaks:
-            if len(streak) > 1:
+            if len(streak) > 2:
                 for kill in streak:
                     temp_events.append(kill)
 
         merged = []
         for streak in kill_streaks:
-            if len(streak) > 6:
+            if len(streak) > 2:
                 merged.append(Event(type="KillStreak",
                     timestart=streak[0]["timestart"],
                     timeend=streak[-1]["timeend"],
@@ -123,9 +99,9 @@ class KillEventsProcessor:
                     kills=len(streak)))
 
         merged = [event.to_dict() for event in merged]
-        # for event in events:
-        #     if event not in temp_events:
-        #         merged.append(event)
+        for event in events:
+            if event not in temp_events:
+                merged.append(event)
         del temp_events, events, kill_streaks, current_streak
 
         with open(f"{self.output_dir}/events_temp_2.json", 'w') as f:
