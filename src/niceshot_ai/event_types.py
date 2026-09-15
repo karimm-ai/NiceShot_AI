8db2e657ae925d367a0621d1c1dd31a8e3426032
unknown <karimm.16199@gmail.com>
2026-09-15 22:26:15 +0300

    refactor: rewrite class with updated structure

--- src/niceshot_ai/event_types.py
+++ src/niceshot_ai/event_types.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 import time
 
 
-@dataclass
+@dataclass(init=False)
 class Event:
     """Attributes for an event"""
 
@@ -13,18 +13,39 @@ class Event:
     timestamp: str = ""
     desc: str = ""
     kills: int = 0
-    
+
+    def __init__(
+        self,
+        type: str,
+        timestart: float,
+        timeend: float,
+        video_num: int,
+        timestamp: str = "",
+        desc: str = "",
+        kills: int = 0,
+
+        **kwargs
+    ):
+        self.type = type
+        self.timestart = timestart
+        self.timeend = timeend
+        self.video_num = video_num
+        self.timestamp = timestamp
+        self.desc = desc
+        self.kills = kills
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+        self.__post_init__()
+
     def __post_init__(self):
-        self.timestamp = time.strftime("%H:%M:%S", time.gmtime(self.timestart))
-        self.timestamp = self.timestamp.replace(":", ".")
-        self.desc = f"{self.type}in{str(self.video_num)}@{self.timestamp}.mp4"
+        self.timestamp = time.strftime(
+            "%H:%M:%S",
+            time.gmtime(self.timestart)
+        ).replace(":", ".")
+
+        self.desc = f"{self.type}in{self.video_num}@{self.timestamp}.mp4"
 
     def to_dict(self) -> dict:
-        return {
-            "type": self.type,
-            "timestart": self.timestart,
-            "timeend": self.timeend,
-            "timestamp": self.timestamp,
-            "desc": self.desc,
-            "kills": self.kills
-        }
+        return self.__dict__.copy()
